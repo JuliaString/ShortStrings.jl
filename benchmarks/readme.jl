950caffba847ea928877ea1dcabd9041fc44b684
susabi <zhuojia.dai@gmail.com>
2018-01-28 22:42:22 +1100

    repeated strings benchmark

--- benchmarks/readme.jl
+++ benchmarks/readme.jl
@@ -21,3 +21,28 @@ bar(["Base.sort","SortingLab.radixsort","ShortStrings radix sort", "R radix sort
     title="String sort performance - len: 1m, variable size 15",
     label = "seconds")
 savefig("readme_string_sort.png")
+
+
+# short be
+using SortingLab, ShortStrings, SortingAlgorithms, BenchmarkTools;
+N = Int(2e7);
+svec = rand([randstring(rand(1:15)) for i=1:N÷100],N)
+# convert to ShortString
+ssvec = ShortString.(svec);
+basesort = @benchmark sort($svec) samples = 5 seconds = 120
+radixsort_timings = @benchmark SortingLab.radixsort($svec) samples = 5 seconds = 120
+short_radixsort = @benchmark sort($ssvec, by = x->x.size_content, alg=RadixSort) samples = 5 seconds = 120
+
+using RCall
+
+@rput svec;
+r_timings = R"""
+replicate(max(5, $(length(short_radixsort.times))), system.time(sort(svec, method="radix"))[3])
+""";
+
+using Plots
+bar(["Base.sort","SortingLab.radixsort","ShortStrings radix sort", "R radix sort"],
+    mean.([basesort.times./1e9, radixsort_timings.times./1e9, short_radixsort.times./1e9, r_timings]),
+    title="String sort performance - len: $(N÷1_000_000)m, fixed size: 15",
+    label = "seconds")
+savefig("readme_string_sort_fixed_len.png")
