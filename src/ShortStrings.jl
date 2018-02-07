fadeb65b04f31bbbbdcb41728b133d7a3653b03f
susabi <zhuojia.dai@gmail.com>
2018-02-07 23:14:33 +1100

    reorganised into more cohesive whole and included fsort

--- src/ShortStrings.jl
+++ src/ShortStrings.jl
@@ -1,28 +1,30 @@
 module ShortStrings
 
 using SortingAlgorithms
-export ShortString, ShorterString
+export ShortString, ShortString15, ShortString7, ShortString3, fsort
 
-struct ShortString <: AbstractString
-    size_content::UInt128
+struct ShortString{T} <: AbstractString where T
+    size_content::T
 
-    function ShortString(s::String)
+    function ShortString{T}(s::String) where T
         sz = sizeof(s)
-        if sz > 15
-            throw(ErrorException("sizeof(::ShortString) must be shorter than or equal to 15 in length; \
+        if sz > sizeof(T) - 1 # the last byte is used to store the length
+            throw(ErrorException("sizeof(::ShortString) must be shorter than or equal to $(sizeof(T) - 1) in length;
             you have supplied a string of size $sz"))
         end
 
-        content = (UInt128(s |> pointer |> Ptr{UInt128} |> Base.unsafe_load |> ntoh) >> 8(16 - sz)) << 8(16 - sz)
+        bits_to_wipe = 8(sizeof(T) - sz)
 
-        new(content | UInt128(sz))
+        content = (T(s |> pointer |> Ptr{T} |> Base.unsafe_load |> ntoh) >> bits_to_wipe) << bits_to_wipe
+
+        new(content | T(sz))
     end
 
-    ShortString(s::U) where U <: Unsigned = UInt128(s)
+    ShorterString(s) = T(s)
 end
 
 Base.endof(s::ShortString) = Int(s.size_content & 0xf)
-Base.next(s::ShortString, i::Int) = (Char((s.size_content << 8(i-1)) >> 8*15), i + 1)
+Base.next(s::ShortString{T}, i::Int) where T = (Char((s.size_content << 8(i-1)) >> 8*(sizeof(T)-1)), i + 1)
 Base.sizeof(s::ShortString) = Int(s.size_content & 0xf)
 Base.print(s::ShortString) = print(s.size_content)
 Base.display(s::ShortString) = display(s.size_content)
@@ -30,32 +32,41 @@ Base.convert(::ShortString, s::String) = ShortString(s)
 Base.convert(::String, ss::ShortString) = reduce(*, ss)
 Base.start(::ShortString) = 1
 
-struct ShorterString <: AbstractString
-    size_content::UInt64
+size_content(s::ShortString) = s.size_content
 
-    function ShorterString(s::String)
-        sz = sizeof(s)
-        if sz > 7
-            throw(ErrorException("sizeof(::ShorterString) must be shorter than or equal to 7 in length; \
-            you have supplied a string of size $sz"))
-        end
+const ShortString15 = ShortString{UInt128}
+const ShortString7 = ShortString{UInt64}
+const ShortString3 = ShortString{UInt32}
 
-        content = (UInt64(s |> pointer |> Ptr{UInt64} |> Base.unsafe_load |> ntoh) >> 8(8 - sz)) << 8(8 - sz)
 
-        new(content | UInt64(sz))
-    end
+fsort(v::Vector{ShortString{T}}; rev = false) where T = sort(v, rev = rev, by = size_content, alg = RadixSort)
 
-    ShorterString(s) = UInt64(s)
-end
+# struct ShorterString <: AbstractString
+#     size_content::UInt64
 
-Base.endof(s::ShorterString) = Int(s.size_content & 0xf)
-Base.next(s::ShorterString, i::Int) = (Char((s.size_content << 8(i-1)) >> 8*7), i + 1)
-Base.sizeof(s::ShorterString) = Int(s.size_content & 0xf)
-Base.print(s::ShorterString) = print(s.size_content)
-Base.display(s::ShorterString) = display(s.size_content)
-Base.convert(::ShorterString, s::String) = ShorterString(s)
-Base.convert(::String, ss::ShorterString) = reduce(*, ss)
-Base.start(::ShorterString) = 1
+#     function ShorterString(s::String)
+#         sz = sizeof(s)
+#         if sz > 7
+#             throw(ErrorException("sizeof(::ShorterString) must be shorter than or equal to 7 in length; \
+#             you have supplied a string of size $sz"))
+#         end
+
+#         content = (UInt64(s |> pointer |> Ptr{UInt64} |> Base.unsafe_load |> ntoh) >> 8(8 - sz)) << 8(8 - sz)
+
+#         new(content | UInt64(sz))
+#     end
+
+#     ShorterString(s) = UInt64(s)
+# end
+
+# Base.endof(s::ShorterString) = Int(s.size_content & 0xf)
+# Base.next(s::ShorterString, i::Int) = (Char((s.size_content << 8(i-1)) >> 8*7), i + 1)
+# Base.sizeof(s::ShorterString) = Int(s.size_content & 0xf)
+# Base.print(s::ShorterString) = print(s.size_content)
+# Base.display(s::ShorterString) = display(s.size_content)
+# Base.convert(::ShorterString, s::String) = ShorterString(s)
+# Base.convert(::String, ss::ShorterString) = reduce(*, ss)
+# Base.start(::ShorterString) = 1
 
 end # module
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,21 @@ using ShortStrings
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+r = randstring.(1:15)
+@test all(ShortString15.(r) .== r)
+a = ShortString15.(r)
+@test fsort(a) |> issorted
+
+r = randstring.(1:7)
+@test all(ShortString7.(r) .== r)
+a = ShortString7.(r)
+@test fsort(a) |> issorted
+
+r = randstring.(1:3)
+@test all(ShortString{UInt32}.(r) .== r)
+a = ShortString{UInt32}.(r)
+@test fsort(a) |> issorted
+
+@test all(ShortString3.(r) .== r)
+a = ShortString3.(r)
+@test fsort(a) |> issorted
