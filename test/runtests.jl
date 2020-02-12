c0c472213c8eec0a6f514b0e387e5980ae13ae45
Kevin Squire <kevin@secondspectrum.com>
2020-02-12 12:23:40 -0800

    Export and test string macros

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,6 @@
 using ShortStrings
 using Test, Random
 
-# write your own tests here
 r = randstring.(1:15)
 @test all(ShortString15.(r) .== r)
 a = ShortString15.(r)
@@ -21,6 +20,20 @@ a = ShortString{UInt32}.(r)
 a = ShortString3.(r)
 @test fsort(a) |> issorted
 
-
-
 @test collect(ShortString3("abc")) == ['a', 'b', 'c']
+
+@test ss15"Short String!!!" === ShortString15("Short String!!!")
+@test ss7"ShrtStr" === ShortString7("ShrtStr")
+@test ss3"ss3" === ShortString3("ss3")
+
+@test_throws ErrorException ShortString{UInt128}("Short String!!!!")
+@test_throws ErrorException ShortString15("Short String!!!!")
+@test_throws ErrorException ss15"Short String!!!!"
+
+@test_throws ErrorException ShortString{UInt64}("ShortStr")
+@test_throws ErrorException ShortString7("ShortStr")
+@test_throws ErrorException ss7"ShortStr"
+
+@test_throws ErrorException ShortString{UInt32}("Short")
+@test_throws ErrorException ShortString3("Short")
+@test_throws ErrorException ss3"Short"
