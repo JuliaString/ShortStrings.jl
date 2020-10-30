f9c6d25dd86e215117f3ef9ed70c7c5e42065a55
Lyndon White <lyndon.white@invenialabs.co.uk>
2020-10-30 22:00:30 +0000

    Constructing one ShortString from another

--- src/base.jl
+++ src/base.jl
@@ -6,13 +6,16 @@ struct ShortString{T} <: AbstractString where T
     size_content::T
 end
 
+function check_size(T, sz)
+    max_len = sizeof(T) - size_nibbles(T)  # the last few nibbles are is used to store the length
+    if sz > max_len
+        throw(ErrorException("sizeof(::ShortString) must be shorter than or equal to $(max_len) in length; you have supplied a string of size $sz"))
+    end
+end
 
 function ShortString{T}(s::Union{String, SubString{String}}) where T
     sz = sizeof(s)
-    max_len = sizeof(T) - size_nibbles(T)
-    if sz > max_len # the last byte is used to store the length
-        throw(ErrorException("sizeof(::ShortString) must be shorter than or equal to $(max_len) in length; you have supplied a string of size $sz"))
-    end
+    check_size(T, sz)
     bits_to_wipe = 8(sizeof(T) - sz)
     # TODO some times this can throw errors for longish strings
     # Exception: EXCEPTION_ACCESS_VIOLATION at 0x1e0b7afd -- bswap at C:\Users\RTX2080\.julia\packages\BitIntegers\xU40U\src\BitIntegers.jl:332 [inlined]
@@ -20,6 +23,16 @@ function ShortString{T}(s::Union{String, SubString{String}}) where T
     content = (T(s |> pointer |> Ptr{T} |> Base.unsafe_load |> ntoh) >> bits_to_wipe) << bits_to_wipe
     ShortString{T}(content | T(sz))
 end
+
+ShortString{T}(s::ShortString{T}) where T = s
+function ShortString{T}(s::ShortString{S}) where {T, S}
+    sz = sizeof(s)
+    check_size(T, sz)
+    # Flip it so empty bytes are at start, grow/shrink it, flip it back
+    content = ntoh(T(ntoh(s.size_content & ~S(size_mask(S)))))
+    ShortString{T}(content | T(sz))
+end
+
 
 String(s::ShortString) = String(reinterpret(UInt8, [s.size_content|>ntoh])[1:sizeof(s)])
 
