3d7b5a4bff8e837fe4ea976f2f5b9e163bf4e4b7
Jacob Quinn <quinn.jacobd@gmail.com>
2021-04-26 12:57:28 -0600

    Add additional test cases to improve coverage

--- src/ShortStrings.jl
+++ src/ShortStrings.jl
@@ -3,7 +3,7 @@ module ShortStrings
 using BitIntegers
 using SortingAlgorithms
 
-export fsort, fsort!, ShortString, ShortString3, ShortString7, ShortString15
+export fsort, fsort!, fsortperm, ShortString, ShortString3, ShortString7, ShortString15
 export ShortString31, ShortString63, ShortString127, ShortString255
 export @ss_str, @ss3_str, @ss7_str, @ss15_str, @ss31_str, @ss63_str, @ss127_str, @ss255_str
 
